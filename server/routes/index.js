7f73989aed3c5372d2a3bfb1b0a138f8206d941f
Moritz Miedler <71124607+MoritzMiedler@users.noreply.github.com>
2021-04-21 10:49:36 +0200

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 // enter your code here
 
 router.post('/login', (req, res) => {
-  HILFE;
+  let email = req.body.email;
+  let password = req.body.password;
+  if (email && password) {
+    const user = users.find(el => el.email === email && el.password === password);
+    if (user) {
+      req.session.userId = user.id;
+      res.status(200).json({ id: user.id, name: user.name });
+    } else res.status(401).send('Wrong email or password');
+  } else res.status(400).send('Login failed');
 });
 
 router.get('/logout', redirectLogin, (req, res) => {
